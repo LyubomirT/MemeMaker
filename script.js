b14b9a7a63524dea1333bbb9348c23dcb89afb0b
Lyubomir <scratchwix@gmail.com>
2023-03-08 15:06:48 +0200

    Config DOM

---
+++ script.js
@@ -0,0 +1,6 @@
+copyButton = document.getElementById("copyButton");
+undoButton = document.getElementById("undoButton");
+redoButton = document.getElementById("redoButton");
+saveButton = document.getElementById("saveButton");
+loadButton = document.getElementById("loadButton");
+exportButton = document.getElementById("exportButton");
